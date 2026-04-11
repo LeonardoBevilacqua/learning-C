337297dc66ff6c00d890107a621b4b7458bc7994
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-11 14:19:12 -0300

    [conditions and loops] video 05

--- part-01/guessing-game/guessing-game.c
+++ part-01/guessing-game/guessing-game.c
@@ -13,16 +13,18 @@ int main() {
     scanf("%d", &guess);
     printf("Your guess was %d\n", guess);
 
-    if (guess == secret_number) {
+    int is_correct = guess == secret_number;
+
+    if (is_correct) {
         printf("Congratulations! You're correct!\n");
         printf("Play again, you're a good player!\n");
     } else {
 
-        if (guess > secret_number) {
-            printf("Your guess was greater than the secret number\n");
-        }
+        int is_gretter = guess > secret_number;
 
-        if (guess < secret_number) {
+        if (is_gretter) {
+            printf("Your guess was greater than the secret number\n");
+        } else {
             printf("Your guess was less than the secret number\n");
         }
     }
