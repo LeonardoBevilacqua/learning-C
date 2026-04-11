5771c7e4a9ad804272e297b0d027c7b6c268ae60
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-11 12:31:58 -0300

    [variables] video 06

---
+++ part-01/guessing-game/guessing-game.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // print the header of our game
+    printf("********************************\n");
+    printf("* Welcome to our guessing game *\n");
+    printf("********************************\n");
+}
--- part-01/guessing-game/main.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Welcome to our guessing game");
-}
