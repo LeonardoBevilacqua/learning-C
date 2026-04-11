7ee2a7b0971cff83154b323056861678d4e5aa50
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-11 12:38:11 -0300

    [variables] video 07

--- part-01/guessing-game/guessing-game.c
+++ part-01/guessing-game/guessing-game.c
@@ -5,4 +5,8 @@ int main() {
     printf("********************************\n");
     printf("* Welcome to our guessing game *\n");
     printf("********************************\n");
+
+    int secret_number = 42;
+
+    printf("The number %d is the secret number. Don't tell anyone!", secret_number);
 }
