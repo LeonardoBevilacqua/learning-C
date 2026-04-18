a756b3eb8efb5270b777221652e3df09a2f6a72b
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-18 14:18:23 -0300

    [arrays] video 01-2

--- part-02/word-guess.c
+++ part-02/word-guess.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char character1 = 'M';
-    char character2 = 'E';
-    char character3 = 'L';
-    char character4 = 'A';
-    char character5 = 'N';
-    char character6 = 'C';
-    char character7 = 'I';
-    char character8 = 'A';
+    int notas[10];
 
-    printf("%c%c%c%c%c%c%c%c\n", character1, character2, character3,
-            character4, character5, character6, character7, character8);
+    notas[0] = 10;
+    notas[2] = 9;
+    notas[3] = 8;
+    notas[9] = 4;
+
+    printf("notas %d %d %d", notas[0], notas[2], notas[9]);
 }
