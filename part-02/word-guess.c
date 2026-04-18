69ff3b8662a9293d3f571a9a6c2ae6c8f08b10cf
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-18 14:25:14 -0300

    [arrays] video 02-1

--- part-02/word-guess.c
+++ part-02/word-guess.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int notas[10];
+    char secrect_word[20];
 
-    notas[0] = 10;
-    notas[2] = 9;
-    notas[3] = 8;
-    notas[9] = 4;
+    secrect_word[0] = 'M';
+    secrect_word[1] = 'E';
+    secrect_word[2] = 'L';
+    secrect_word[3] = 'A';
+    secrect_word[4] = 'N';
+    secrect_word[5] = 'C';
+    secrect_word[6] = 'I';
+    secrect_word[7] = 'A';
 
-    printf("notas %d %d %d", notas[0], notas[2], notas[9]);
+    printf("%c%c%c%c%c%c%c%c\n",
+            secrect_word[0],
+            secrect_word[1],
+            secrect_word[2],
+            secrect_word[3],
+            secrect_word[4],
+            secrect_word[5],
+            secrect_word[6],
+            secrect_word[7]);
 }
