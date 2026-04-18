0c806b1443eb2391e027abd86ec2ed5569bfeabe
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-18 14:29:02 -0300

    [arrays] video 02-2

--- part-02/word-guess.c
+++ part-02/word-guess.c
@@ -3,22 +3,7 @@
 int main() {
     char secrect_word[20];
 
-    secrect_word[0] = 'M';
-    secrect_word[1] = 'E';
-    secrect_word[2] = 'L';
-    secrect_word[3] = 'A';
-    secrect_word[4] = 'N';
-    secrect_word[5] = 'C';
-    secrect_word[6] = 'I';
-    secrect_word[7] = 'A';
+    sprintf(secrect_word, "MELANCIA");
 
-    printf("%c%c%c%c%c%c%c%c\n",
-            secrect_word[0],
-            secrect_word[1],
-            secrect_word[2],
-            secrect_word[3],
-            secrect_word[4],
-            secrect_word[5],
-            secrect_word[6],
-            secrect_word[7]);
+    printf("%s\n", secrect_word);
 }
