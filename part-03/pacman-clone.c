8c256b2cfa307f14566d822fea47ebb809e4f311
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-25 10:44:16 -0300

    [vector] video 04

--- part-03/pacman-clone.c
+++ part-03/pacman-clone.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    // vector of 5 x 10
-    char map[5][10];
+    // vector of 5 x 10. The '+ 1' is to include the end of line
+    char map[5][10+1];
 
-    map[0][0] = '|';
-    map[4][9] = '@';
+    FILE* file = fopen("map.txt", "r");
+    if (file == 0) {
+        printf("Error while readiing the map file\n");
+        exit(1);
+    }
 
-    printf("%c %c\n", map[0][0], map[4][9]);
+    for (int i = 0; i < 5; i++) {
+        fscanf(file, "%s", map[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%s\n", map[i]);
+    }
+
+    fclose(file);
 }
