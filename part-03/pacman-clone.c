36aab1dc6cf8d14ba779d30f6b4dec70335669cd
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-25 10:59:59 -0300

    [vector] video 07-1

--- part-03/pacman-clone.c
+++ part-03/pacman-clone.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    // vector of 5 x 10. The '+ 1' is to include the end of line
-    char map[5][10+1];
+char map[5][10+1];
+int rows;
+int columns;
 
+int main() {
     FILE* file = fopen("map.txt", "r");
     if (file == 0) {
         printf("Error while readiing the map file\n");
         exit(1);
     }
+
+    fscanf(file, "%d %d", &rows, &columns);
+    printf("Rows %d Columns %d\n", rows, columns);
+
+    /////////////
+    int** v = malloc(sizeof(int*) * 5);
+    for (int i = 0; i < 5; i++) {
+        v[i] = malloc(sizeof(int) * 10);
+    }
+    v[0][0] = 10;
+    v[1][2] = 12;
+    printf("allocated int %d %d\n", v[0][0], v[1][2]);
+    for (int i = 0; i < 5; i++) {
+        free(v[i]);
+    }
+    free(v);
+    /////////////
 
     for (int i = 0; i < 5; i++) {
         fscanf(file, "%s", map[i]);
