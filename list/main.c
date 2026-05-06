b3b8a82208db086ca97c3c909e2e1b589590986f
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-05-06 15:12:57 -0300

    removing manual

--- list/main.c
+++ list/main.c
@@ -12,35 +12,17 @@ void print_list(NODE* list) {
 }
 
 int main() {
-    NODE* head = malloc(sizeof(NODE));
-    NODE* second = malloc(sizeof(NODE));
-    NODE* third = malloc(sizeof(NODE));
-
-    if (!head || !second || !third) {
-        printf("Memory allocation failed\n");
-        return 1;
-    }
-
-    head->data = 10;
-    head->next = second;
-
-    second->data = 20;
-    second->next = third;
-    
-    third->data =30;
-    third->next = NULL;
-
-    print_list(head);
-    free_list(head);
-
     NODE* my_list = NULL;
+
     add(10, &my_list);
     add(20, &my_list);
     add(30, &my_list);
     print_list(my_list);
+
     add(40, &my_list);
     print_list(my_list);
 
     free_list(my_list);
+
     return 0;
 }
