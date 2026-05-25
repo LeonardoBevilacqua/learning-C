150813ceefbc741d463de551c9db3d8a0da8f0f0
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-05-25 15:10:40 -0300

    using malloc

--- pathing/main.c
+++ pathing/main.c
@@ -1,44 +1,43 @@
 #include "path.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+#define MAX_NODES 8
 /*
  * A - B - C - E - G - H
  *       |             |
  *       - D - F - - - -
 */
 int main() {
-    NODE nodeA = create_node('A');
-    NODE nodeB = create_node('B');
-    NODE nodeC = create_node('C');
-    NODE nodeD = create_node('D');
-    NODE nodeE = create_node('E');
-    NODE nodeF = create_node('F');
-    NODE nodeG = create_node('G');
-    NODE nodeH = create_node('H');
+    NODE* nodes = malloc(sizeof(NODE) * MAX_NODES);
+    char values[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
 
-    connect_next_nodes(&nodeA, (NODE*[]){ &nodeB, NULL });
+    for (int i = 0; i < MAX_NODES; i++) nodes[i] = create_node(values[i]);
 
-    connect_previous_nodes(&nodeB, (NODE*[]){ &nodeA, NULL });
-    connect_next_nodes(&nodeB, (NODE*[]){ &nodeC, &nodeD });
+    connect_next_nodes(&nodes[0], (NODE*[]){ &nodes[1], NULL });
 
-    connect_previous_nodes(&nodeC, (NODE*[]){ &nodeB, NULL });
-    connect_next_nodes(&nodeC, (NODE*[]){ &nodeE, NULL });
+    connect_previous_nodes(&nodes[1], (NODE*[]){ &nodes[0], NULL });
+    connect_next_nodes(&nodes[1], (NODE*[]){ &nodes[2], &nodes[3] });
 
-    connect_previous_nodes(&nodeD, (NODE*[]){ &nodeB, NULL });
-    connect_next_nodes(&nodeD, (NODE*[]){ &nodeF, NULL });
+    connect_previous_nodes(&nodes[2], (NODE*[]){ &nodes[1], NULL });
+    connect_next_nodes(&nodes[2], (NODE*[]){ &nodes[4], NULL });
 
-    connect_previous_nodes(&nodeE, (NODE*[]){ &nodeC, NULL });
-    connect_next_nodes(&nodeE, (NODE*[]){ &nodeG, NULL });
+    connect_previous_nodes(&nodes[3], (NODE*[]){ &nodes[1], NULL });
+    connect_next_nodes(&nodes[3], (NODE*[]){ &nodes[5], NULL });
 
-    connect_previous_nodes(&nodeF, (NODE*[]){ &nodeD, NULL });
-    connect_next_nodes(&nodeF, (NODE*[]){ &nodeH, NULL });
+    connect_previous_nodes(&nodes[4], (NODE*[]){ &nodes[2], NULL });
+    connect_next_nodes(&nodes[4], (NODE*[]){ &nodes[6], NULL });
 
-    connect_previous_nodes(&nodeG, (NODE*[]){ &nodeE, NULL });
-    connect_next_nodes(&nodeG, (NODE*[]){ &nodeH, NULL });
+    connect_previous_nodes(&nodes[5], (NODE*[]){ &nodes[3], NULL });
+    connect_next_nodes(&nodes[5], (NODE*[]){ &nodes[7], NULL });
 
-    connect_previous_nodes(&nodeH, (NODE*[]){ &nodeG, &nodeF });
+    connect_previous_nodes(&nodes[6], (NODE*[]){ &nodes[4], NULL });
+    connect_next_nodes(&nodes[6], (NODE*[]){ &nodes[7], NULL });
 
-    navigate_nodes(&nodeA);
+    connect_previous_nodes(&nodes[7], (NODE*[]){ &nodes[6], &nodes[5] });
 
+    navigate_nodes(nodes);
+
+    free(nodes);
     return 0;
 }
